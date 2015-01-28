4dde847db2583c4511a61c5c7804804cc26a4a7d
Carl Osterwisch <costerwi@gmail.com>
2015-01-28 13:36:42 -0500

    Accept outside temperature and humidity from Nest
    This saves extra fetching and processing from wunderground.

--- resources/utils/nest-get-data.php
+++ resources/utils/nest-get-data.php
@@ -19,7 +19,7 @@ mysql_select_db($dbname);
 //Create a new Nest Object
 $nest = new Nest();
 
-//Used to return current location and away status
+//Used to return current location, away status, and outside weather
 $locations = $nest->getUserLocations();
 
 //Postal Code formatting
@@ -34,19 +34,8 @@ if ($datatype === 'current'){
 	//Capture date and time of script execution
 	$runTime = $date = date('Y-m-d H:i:s');
 
-	//Used to get current outdoor weather
-	$weather_json = file_get_contents('http://api.wunderground.com/api/'.$wu_api_key.'/conditions/q/'.rawurlencode($postal_code).'.json');
-	$weather=json_decode($weather_json);
-
 	//Used to return current inside temperature, current inside humidity, current mode, target temperature, time to target temperature, current heat state, current ac state
 	$infos = $nest->getDeviceInfo();
-	
-	//Determine if we need Celsius temperature
-	if (in_array(date_default_timezone_get(), $us_timezones)) {
-		$current_temp = $weather->current_observation->temp_f; 
-	} else {
-		$current_temp = $weather->current_observation->temp_c; 
-	}
 
 	//If the target temperature is an array, we need to deal with that.
 	if (strpos($infos->current_state->mode,'heat') !== false) {
@@ -71,7 +60,7 @@ if ($datatype === 'current'){
 	}
 	
 	//Insert Current Values into Nest Database Table
-	$query = 'INSERT INTO nest (log_datetime, location, outside_temp, outside_humidity, away_status, leaf_status, current_temp, current_humidity, temp_mode, low_target_temp, high_target_temp, time_to_target, target_humidity, heat_on, humidifier_on, ac_on, fan_on, battery_level, is_online) VALUES ("'.$runTime.'", "'.$postal_code.'", "'.$current_temp.'", "'.$weather->current_observation->relative_humidity.'", "'.$locations[0]->away.'", "'.$infos->current_state->leaf.'", "'.$infos->current_state->temperature.'", "'.$infos->current_state->humidity.'", "'.$infos->current_state->mode.'", "'.$low_target_temp.'", "'.$high_target_temp.'", "'.$infos->target->time_to_target.'","'.$infos->target->humidity.'","'.$infos->current_state->heat.'","'.$infos->current_state->humidifier.'","'.$infos->current_state->ac.'","'.$infos->current_state->fan.'","'.$infos->current_state->battery_level.'","'.$infos->network->online.'")';
+	$query = 'INSERT INTO nest (log_datetime, location, outside_temp, outside_humidity, away_status, leaf_status, current_temp, current_humidity, temp_mode, low_target_temp, high_target_temp, time_to_target, target_humidity, heat_on, humidifier_on, ac_on, fan_on, battery_level, is_online) VALUES ("'.$runTime.'", "'.$postal_code.'", "'.$locations[0]->outside_temperature.'", "'.$locations[0]->outside_humidity.'", "'.$locations[0]->away.'", "'.$infos->current_state->leaf.'", "'.$infos->current_state->temperature.'", "'.$infos->current_state->humidity.'", "'.$infos->current_state->mode.'", "'.$low_target_temp.'", "'.$high_target_temp.'", "'.$infos->target->time_to_target.'","'.$infos->target->humidity.'","'.$infos->current_state->heat.'","'.$infos->current_state->humidifier.'","'.$infos->current_state->ac.'","'.$infos->current_state->fan.'","'.$infos->current_state->battery_level.'","'.$infos->network->online.'")';
 	$result = mysql_query($query);	
 
 	//Set the humidity level if enabled.
